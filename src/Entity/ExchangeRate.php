2de69181465824e74bd13695b08cabd94dd58554
Balvir Sohal <balvir@smartshore.nl>
2023-05-27 14:17:26 +0530

    Generate entity

---
+++ src/Entity/ExchangeRate.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExchangeRateRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExchangeRateRepository::class)]
+class ExchangeRate
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fromCurrency = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $toCurrency = null;
+
+    #[ORM\Column]
+    private ?float $rate = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFromCurrency(): ?string
+    {
+        return $this->fromCurrency;
+    }
+
+    public function setFromCurrency(string $fromCurrency): self
+    {
+        $this->fromCurrency = $fromCurrency;
+
+        return $this;
+    }
+
+    public function getToCurrency(): ?string
+    {
+        return $this->toCurrency;
+    }
+
+    public function setToCurrency(string $toCurrency): self
+    {
+        $this->toCurrency = $toCurrency;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
